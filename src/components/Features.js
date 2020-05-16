169328bf174449a67ccab546902d979ac997a0a2
Nabeela Hussain <nrh-91@hotmail.co.uk>
2020-05-16 18:38:42 +0100

    Fix image height

--- src/components/Features.js
+++ src/components/Features.js
@@ -10,7 +10,7 @@ const FeatureGrid = ({ gridItems }) => (
           <div className="has-text-centered">
             <div
               style={{
-                width: '240px',
+                height: '240px',
                 display: 'inline-block',
               }}
             >
