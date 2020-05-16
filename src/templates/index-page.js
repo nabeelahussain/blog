6e945aa8c8b4e41362d6ea0c0446c3f458fdb22e
Nabeela Hussain <nrh-91@hotmail.co.uk>
2020-05-16 18:38:23 +0100

    Change button text

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -86,7 +86,7 @@ export const IndexPageTemplate = ({
                 <div className="columns">
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/products">
-                      See all products
+                      See Artwork
                     </Link>
                   </div>
                 </div>
