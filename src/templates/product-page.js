50ba3b2ed93bc52ae86bfb4614cfa81883721652
Nabeela Hussain <nrh-91@hotmail.co.uk>
2020-05-16 19:48:21 +0100

    Remove redundant pricing section

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -78,21 +78,6 @@ export const ProductPageTemplate = ({
                   </div>
                 </div>
               </div>
-              <div
-                className="full-width-image-container"
-                style={{
-                  backgroundImage: `url(${
-                    fullImage.childImageSharp
-                      ? fullImage.childImageSharp.fluid.src
-                      : fullImage
-                  })`,
-                }}
-              />
-              <h2 className="has-text-weight-semibold is-size-2">
-                {pricing.heading}
-              </h2>
-              <p className="is-size-5">{pricing.description}</p>
-              <Pricing data={pricing.plans} />
             </div>
           </div>
         </div>
